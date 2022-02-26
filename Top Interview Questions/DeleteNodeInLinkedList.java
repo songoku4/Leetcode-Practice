d266ff60921e4ceeb2d9618ce7305128df23c81a
Aaditya Sharma <35288334+songoku4@users.noreply.github.com>
2022-02-26 17:37:50 +0530

    Update DeleteNodeInLinkedList.java

--- Top Interview Questions/DeleteNodeInLinkedList.java
+++ Top Interview Questions/DeleteNodeInLinkedList.java
@@ -1,1 +1,17 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode next = node.next;
+        node.val = next.val;
+        node.next = next.next;
+        
+        
+    }
+}
