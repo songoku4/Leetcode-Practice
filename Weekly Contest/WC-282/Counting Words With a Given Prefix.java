75284677f26b870b6961a39b362dfb397a0bbabb
Aaditya Sharma <35288334+songoku4@users.noreply.github.com>
2022-02-28 11:02:15 +0530

    Update Counting Words With a Given Prefix.java

--- Weekly Contest/WC-282/Counting Words With a Given Prefix.java
+++ Weekly Contest/WC-282/Counting Words With a Given Prefix.java
@@ -1,5 +1,14 @@
 class Solution {
+            int count;
+
     public int prefixCount(String[] words, String pref) {
+        for(String wrd : words){
+            if(wrd.startsWith(pref)){
+                count+=1;
+            }
+        }
+                    return count;
+
         
     }
 }
